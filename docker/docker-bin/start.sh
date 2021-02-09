86b3fe91fe5082f335e61ff465dafa0c88cc80fb
Jun Kurihara <kurihara@ieee.org>
2021-02-09 21:18:09 +0900

    change: use default blocklist in original repo

--- docker/docker-bin/start.sh
+++ docker/docker-bin/start.sh
@@ -5,7 +5,8 @@ DOMAINS_BLACKLIST=/etc/domains-blacklist.txt
 # domain blacklist
 if [ -z "$CUSTOM_DOMAIN_BLACKLIST" ]; then
     echo "generate blacklist"
-    /usr/local/bin/generate-blacklist.sh
+    cd /tmp/domains-blocklist
+    /usr/local/bin/generate-domains-blocklist.py -c /tmp/dnscrypt-proxy/domains-blacklist.conf -a /tmp/dnscrypt-proxy/domains-whitelist.conf -r /tmp/dnscrypt-proxy/domains-time-restricted.conf > /etc/domains-blacklist.txt
 else
     echo "use custom domain blacklist: $CUSTOM_DOMAIN_BLACKLIST"
     cp $CUSTOM_DOMAIN_BLACKLIST $DOMAINS_BLACKLIST
